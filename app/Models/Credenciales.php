418050f5ff580c37255f2d55f7fa4c06f7ad357c
Rafael Alejandro Ayala Castillo <r.alex.ayalac@gmail.com>
2022-04-12 22:05:42 -0600

    primer push

---
+++ app/Models/Credenciales.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Credenciales extends Model
+{   
+    
+    use HasFactory;
+    protected $fillable = [ 'Nombre','Apellidos','ID_Equipo'];
+
+}
